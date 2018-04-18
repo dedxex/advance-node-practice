cbda1c5fd6a7b62bdb7f8057875620e8ff3440fd
Taranjeet Singh <taranjeetplay@gmail.com>
2018-04-18 20:38:11 +0530

    first commit

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://test:test@ds231229.mlab.com:31229/blogsssssssssss',
+  cookieKey: '123123123'
+};
