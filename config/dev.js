2f243a6fdd907003f901b793107f19ac9e0f1feb
Taranjeet Singh <taranjeetplay@gmail.com>
2018-04-21 07:08:41 +0530

    configured complete

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,6 @@ module.exports = {
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://test:test@ds231229.mlab.com:31229/blogsssssssssss',
-  cookieKey: '123123123'
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
