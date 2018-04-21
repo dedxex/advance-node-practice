3adc8ed6cf968fc31f891bafc110f5ea31885472
Taranjeet Singh <taranjeetplay@gmail.com>
2018-04-21 06:55:10 +0530

    configured a little for ci

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,17 @@
 const puppeteer = require('puppeteer');
-test("we can lauch a browser",async () => {
-    const browser = await puppeteer.launch({ headless : false });
-    const page = await browser.newPage();
+
+let browser,page;
+beforeEach( async () => {
+    browser = await puppeteer.launch({ headless : false });
+    page = await browser.newPage();
+    await page.goto("localhost:3000")
+})
+
+afterEach ( async () => {
+    await browser.close();
+})
+
+test("we can lauch a browser", async () => {
+    const text = await page.$eval("a.brand-logo", el => el.innerHTML);
+    expect(text).toEqual("Blogster")
 })
